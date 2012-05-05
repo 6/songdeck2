50399ecf851d15ddd4ed4f5cf4eb4bedfdeb2755
Peter Graham <pg.sners@gmail.com>
2012-05-05 15:53:36 -0400

    add css asset-pack

--- app.rb
+++ app.rb
@@ -5,12 +5,17 @@ class App < Sinatra::Base
   register Sinatra::AssetPack
   assets {
     serve '/js', from: '/javascripts'
+    serve '/css', from: '/stylesheets'
     js :app, '/app.js', [
       '/js/vendor/jquery.js',
       '/js/vendor/underscore.js',
       '/js/vendor/backbone.js',
       '/js/vendor/*.js',
       '/js/*.js'
+    ]
+    css :app, '/app.css', [
+      '/css/vendor/*.css',
+      '/css/*.css'
     ]
     js_compression :yui
     prebuild ENV['RACK_ENV'] == 'production'
