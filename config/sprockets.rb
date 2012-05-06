d66783a3b42e392871ad29df7f526acf3700954a
Peter Graham <pg.sners@gmail.com>
2012-05-05 21:57:21 -0400

    make sprockets code more readable

--- config/sprockets.rb
+++ config/sprockets.rb
@@ -1,12 +1,14 @@
-set :sprockets, Sprockets::Environment.new(settings.root)
+sprockets = Sprockets::Environment.new(settings.root)
 
 configure do
-  settings.sprockets.append_path 'javascripts'
-  settings.sprockets.append_path 'stylesheets'
-  #settings.sprockets.append_path 'images'
+  sprockets.append_path 'javascripts'
+  sprockets.append_path 'stylesheets'
+  #sprockets.append_path 'images'
 end
 
 configure :production, :test do
-  settings.sprockets.css_compressor = YUI::CssCompressor.new
-  settings.sprockets.js_compressor = YUI::JavaScriptCompressor.new
+  sprockets.css_compressor = YUI::CssCompressor.new
+  sprockets.js_compressor = YUI::JavaScriptCompressor.new
 end
+
+set :sprockets, sprockets
